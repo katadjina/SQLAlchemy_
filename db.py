4e4fb19cc82ee88f2aa9392964f426b2d3308ac7
Kasia <kata.drzazgowska@gmail.com>
2023-03-01 23:06:59 +0100

    update

--- db.py
+++ db.py
@@ -9,4 +9,21 @@ Base = declarative_base()
 class Person(Base):
     __tablename__ = "people"
     
-    ssn = Comum("ssn", Integer, primary_key)
+    ssn = Column("ssn", Integer, primary_key=True)
+    firstname = Column("firstname" , String)
+    lastname = Column("lastname", String)
+    gender  = Column("gender", CHAR)
+    age = Column("age", Integer)
+    
+    # constructor
+    
+def __init__ (self, ssn, first, last, gender,  age):
+    self.ssn = ssn
+    self.firstname = first
+    self.lastname = last
+    self.gender = gender
+    self.age = age    
+    
+# function to specify how the person info will be displayed    
+def __repr__(self):
+    return f"({self.ssn}, {self.firstname}, {self.lastname}  , {self.age})"
